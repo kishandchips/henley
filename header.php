04a60d69623fd16b6ec11c1c1777ac24a603e808
franszo <franszo.design@gmail.com>
2013-11-19 20:38:08 +0000

    social buttons
    Signed-off-by: franszo <franszo.design@gmail.com>

--- header.php
+++ header.php
@@ -63,7 +63,7 @@ cc.initialise({
 				<a href="<?php bloginfo( 'url' ); ?>" class="site-logo ir" title="<?php bloginfo( 'name' ); ?> - <?php bloginfo( 'description' ); ?>"><?php bloginfo( 'name' ); ?> - <?php bloginfo( 'description' ); ?></a>	
 				<div id="top-nav" class="clearfix">
 					<div class="socials">
-						<div class="fb-like" data-href="http://www.henley.ac.uk/" data-colorscheme="light" data-layout="button_count" data-action="like" data-show-faces="true" data-send="false"></div>
+						<div class="fb-like" data-href="https://www.facebook.com/henleymba" data-colorscheme="light" data-layout="button_count" data-action="like" data-show-faces="true" data-send="false"></div>
 						<div class="g-plusone" data-size="medium" data-href="<?php bloginfo( 'url' ); ?>"></div>
 						<div class="tweet-button"><a href="https://twitter.com/share" class="twitter-share-button">Tweet</a>
 <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
